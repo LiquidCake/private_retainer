16ea4e9a7ffdd778e09c48e1c9add0dc7a9510c2
liquidcake <email@address.com>
2023-02-11 00:16:45 +0200

    fix

--- scrapper/build/deployment/setup-cron-user.sh
+++ scrapper/build/deployment/setup-cron-user.sh
@@ -4,8 +4,8 @@
 
 crontab -l > user-mycron
 
-echo "1 * * * * /home/$USER/prt/scr-run-scrapper-root-job.sh" >> user-mycron
-echo "1 * * * * /home/$USER/prt/scr-run-scrapper-publish-videos-job.sh" >> user-mycron
+echo "0 */4 * * * /home/$USER/prt/scr-run-scrapper-root-job.sh" >> user-mycron
+echo "30 * * * * /home/$USER/prt/scr-run-scrapper-publish-videos-job.sh" >> user-mycron
 
 crontab user-mycron
 rm user-mycron
