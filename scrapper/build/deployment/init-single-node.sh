5af0f68a927b7cdcfc1758ef2fef5387a9dcf6cf
liquidcake <email@address.com>
2023-02-11 02:55:13 +0200

    env fixes

--- scrapper/build/deployment/init-single-node.sh
+++ scrapper/build/deployment/init-single-node.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 sudo mkdir -p /var/prt-scrapper-data/video_preview
-
 sudo chmod 777 -R /var/prt-scrapper-data
+
+#env variables are not available in cron, even $USER, so we need helper file with static path
+sudo echo USER=$USER > /var/prt-scrapper-data/prt-static-env
 
 #disable web servers if installed
 sudo service nginx stop
--- scrapper/build/deployment/scr-run-scrapper-root-job.sh
+++ scrapper/build/deployment/scr-run-scrapper-root-job.sh
@@ -1,2 +1,4 @@
-#!/bin/sh
+#!/bin/bash -l
+source /var/prt-scrapper-data/prt-static-env
+source /home/$USER/prt/.env
 curl 127.0.0.1:10339/start-scrapper-job-root -u ${HTTP_BASIC_AUTH_USERNAME}:${HTTP_BASIC_AUTH_PASSWORD}
