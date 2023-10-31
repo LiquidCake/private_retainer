d75c36d2dd80f93989c2b3269ac55a831a4c4dec
LiquidCake <han132321@gmail.com>
2023-11-01 00:51:28 +0200

    Update run_webapp.py

--- scrapper/prt_scrapper/run_webapp.py
+++ scrapper/prt_scrapper/run_webapp.py
@@ -5,12 +5,14 @@ if not 'PRT_ENV' in os.environ:
     os.environ['PRT_ENV'] = 'dev'
 
 debug = False
+processes = 4
 
 if os.environ['PRT_ENV'] == 'dev':
      os.environ['ENV_FILE_PATH'] = '.env.local'
      debug = True
+     processes = 1
 
 from scrapper_webapp import app
 
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=8080, debug=debug, threaded=False, processes=4)
+     app.run(host='0.0.0.0', port=8080, debug=debug, threaded=False, processes=processes)
